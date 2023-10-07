1594c0882eca7123838a95d46f7fd33017023bf4
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-10-07 18:22:32 +0000

    Using date widget in the forms and applying date validations

--- tracker/forms.py
+++ tracker/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from djrichtextfield.widgets import RichTextWidget
+from django.forms.widgets import DateInput
+from datetime import date
 from .models import Tracker
 
 
@@ -13,6 +15,10 @@ class TrackerForm(forms.ModelForm):
 
         description = forms.CharField(widget=RichTextWidget())
 
+        widgets = {
+            'date': DateInput(attrs={'type': 'date', 'max': date.today().strftime('%Y-%m-%d')}),
+        }
+
         labels = {
             'title': 'Name of food or meal',
             'date': 'Date when the food was consumed',
@@ -21,3 +27,9 @@ class TrackerForm(forms.ModelForm):
             'image': 'Image',
             'image_alt': 'Describe Image'      
         }
+
+        def clean_date(self):
+            selected_date = self.cleaned_data.get('date')
+            if selected_date and selected_date > date.today():
+                raise forms.ValidationError("The date cannot be in the future.")
+            return selected_date
