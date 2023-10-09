52eb4c935c20caa29b651e9459529a861529b7d3
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-10-09 20:30:22 +0000

    Adding the total calories per day

--- tracker/views.py
+++ tracker/views.py
@@ -5,6 +5,9 @@ from django.views.generic import CreateView, ListView
 from .models import Tracker
 from .forms import TrackerForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from itertools import groupby
+from operator import attrgetter
+
 
 class Trackers(ListView):
     """ View all food """
@@ -12,8 +15,31 @@ class Trackers(ListView):
     model = Tracker
     context_object_name = 'trackers'
 
-    def get_queryset(self):
-        return Tracker.objects.all().order_by('-date')   
+    # Return data ordered by date
+    # def get_queryset(self):
+    #    return Tracker.objects.all().order_by('-date') 
+  
+    # Grouping data entered by date
+    # def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    grouped_by_date = {}
+    #    for date, group in groupby(self.object_list.order_by('-date'), lambda x: x.date):
+    #        grouped_by_date[date] = list(group)
+    #    context['trackers_grouped'] = grouped_by_date
+    #    return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        grouped_by_date = {}
+        
+        for date, group in groupby(self.object_list.order_by('-date'), lambda x: x.date):
+            grouped_entries = list(group)
+            total_calories = sum(entry.calories or 0 for entry in grouped_entries)
+            grouped_by_date[date] = {'entries': grouped_entries, 'total_calories': total_calories}
+        
+        context['trackers_grouped'] = grouped_by_date
+        return context
+
 
 # Create your views here.
 class AddTracker(LoginRequiredMixin, CreateView):
