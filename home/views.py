552164377828eda08da75b34ece93594c7a7315b
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-10-12 18:36:21 +0000

    Creating home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
-from django.views.generic import TemplateView
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic import ListView
+from tracker.models import Tracker
+from django.shortcuts import render
+
 
 # Create your views here.
 
-class Index(TemplateView):
+class Index(ListView):
     template_name = 'home/index.html'
+    model = Tracker
+    context_object_name = 'trackers'
+
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
+    
