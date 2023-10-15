c8faa6d7319cdf0d020e0da5a9177a3ca04ab98b
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-10-15 19:45:13 +0000

    added graphical represntation in the home page

--- home/views.py
+++ home/views.py
@@ -3,6 +3,7 @@ from django.db.models.query import QuerySet
 from django.views.generic import ListView
 from tracker.models import Tracker
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
 
 # Create your views here.
@@ -11,7 +12,26 @@ class Index(ListView):
     template_name = 'home/index.html'
     model = Tracker
     context_object_name = 'trackers'
+    print("Entered home function")
 
     def get_queryset(self):
         return self.model.objects.all()[:3]
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calories = []
+        dates = []
+
+        if self.request.user.is_authenticated:
+            # Fetch data for the last 7 days
+            profile = self.request.user.profile
+            start_date = datetime.now() - timedelta(days=7)
+            trackers = Tracker.objects.filter(user=profile, date__gte=start_date).order_by('date')
+
+            for tracker in trackers:
+                calories.append(tracker.calories)
+                dates.append(tracker.date.strftime('%Y-%m-%d'))
+
+        context['calories'] = calories
+        context['dates'] = dates
+        return context
