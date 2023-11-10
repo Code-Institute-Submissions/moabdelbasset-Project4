452400d1385d6a182511f5b88c008a19a01fb67e
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-11-10 19:36:42 +0000

    fixing the static files in the settings.py

--- main/settings.py
+++ main/settings.py
@@ -186,7 +186,7 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # if 'DEVELOPMENT' in os.environ:
 #     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # else:
