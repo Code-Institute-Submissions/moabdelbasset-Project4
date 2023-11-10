3df4ddf03a19d3acaac2cdbeb92aa5e0a439d8e4
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-11-10 19:54:53 +0000

    fixing the static files in the settings.py

--- main/settings.py
+++ main/settings.py
@@ -189,15 +189,10 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# if 'DEVELOPMENT' in os.environ:
-#     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 # Cloudinary Settings
 
