2532ac1c400273ea74d6dc7dc28dd6d6cca8c2f0
Mohamed Ayman <59439627+moabdelbasset@users.noreply.github.com>
2023-11-10 19:34:45 +0000

    modifying the static files in the settings.py

--- main/settings.py
+++ main/settings.py
@@ -186,15 +186,22 @@ LOGIN_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-if 'DEVELOPMENT' in os.environ:
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = '/static/'
+# if 'DEVELOPMENT' in os.environ:
+#     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # STATIC_URL = "static/"
 
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+
+# Where Django collects static files from
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+# Where Django stores the collected static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Cloudinary Settings
 
